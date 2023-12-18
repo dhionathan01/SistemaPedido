b7fd98e05826455307ce2d8cd11de8f8237498cd
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-12-18 12:00:58 -0300

    build: :building_construction: (SQL) Estrutura: Usuarios
    SQL para Criação dos usuarios

---
+++ SQL/usuarios.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `senha` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
