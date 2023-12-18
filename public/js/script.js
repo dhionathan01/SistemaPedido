e6ce0b1e358894fd7124e62ba0066d50d658d840
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-12-18 11:32:12 -0300

    feat: :sparkles: Preenchimento do formulário / Tratativas de erro ao comunicar api

--- public/js/script.js
+++ public/js/script.js
@@ -10,8 +10,32 @@ function realizarPedido() {
     })
 }
 function obterInformacoesCEP() {
-    let cep = $('#cep').val();
-    $.get(`https://viacep.com.br/ws/${cep}/json/`, function(endereco) {
-        console.log(endereco);
-    });
+    try {
+        let cep = $('#cep').val();
+        if (cep.length == 9) {
+            $.get(`https://viacep.com.br/ws/${cep}/json/`, function(endereco) {
+              // Verifica se o CEP foi encontrado
+              if (endereco.erro) {
+                throw new Error('CEP não encontrado');
+                }
+                // Preenche os campos do formulário com as informações
+                $("#rua").val(endereco.logradouro);
+                $("#bairro").val(endereco.bairro);
+                $("#cidade").val(endereco.localidade);
+                $("#uf").val(endereco.uf);
+
+            });
+        } else {
+            throw new Error('CEP Inválido');
+        }
+    } catch (error) {
+      // Exibe um toast de erro caso ocorra uma exceção
+        toastr.error('CEP inválido ou não encontrado');
+        $('#cep').val('');
+    }
+  }
+function mascaraCEP(cep) {
+    cep = cep.replace(/\D/g, ''); // Remove caracteres não numéricos
+    cep = cep.replace(/^(\d{5})(\d)/, '$1-$2'); // Adiciona o hífen após os primeiros 5 dígitos
+    return cep;
   }
