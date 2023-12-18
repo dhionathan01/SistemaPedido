4f4e6d00c38afccd336ba99d3170e22987f4ffa3
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-12-18 12:06:27 -0300

    build: :building_construction: (SQL) Estrutura: Enderecos
    SQL para Criação dos enderecos

---
+++ SQL/enderecos.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `sistema_pedidos_swl`.`enderecos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `cep` VARCHAR(255) NOT NULL,
+  `uf` VARCHAR(2) NULL,
+  `cidade` VARCHAR(255) NULL,
+  `bairro` VARCHAR(255) NULL,
+  `rua` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`));
